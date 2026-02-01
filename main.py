21e5a5629e2e1caef99eda71baa4d8c07cd558c7
nine1ewenw-ww <gajvoronskijan9@gmail.com>
2026-02-01 11:11:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+API_KEY = "fde54e9c463f4b81ab76f82be0540a14"
+
+BAZA_URL = "https://api.rawg.io/api/games"  
+
+params = {"key": API_KEY, "genres": 4, "page_size": 5}
+
+try:
+    response = requests.get(BAZA_URL, params=params)
+    response.raise_for_status()
+    games = response.json().get("results", [])
+except requests.RequestException as err:
+    print(f"ошибка cписка игр: {err}")
+    exit()
+
+for game in games:
+    game_id = game.get("id")
+    game_name = game.get("name")
+    slug = game.get("slug")
+    released = game.get("released")
+
+    print(f"\n{game_name} ({released})")
+    print(f"https://rawg.io/games/{slug}")
+
+    ss_resp = requests.get(f"{BAZA_URL}/{game_id}/screenshots", params={"key": API_KEY})
+    ss_resp.raise_for_status()
+    screenshots = ss_resp.json().get("results", [])
+    
+    stores_resp = requests.get(f"{BAZA_URL}/{game_id}/stores", params={"key": API_KEY})
+    stores_resp.raise_for_status()
+    stores = stores_resp.json().get("results", [])
+    print("Где купить:")
+    for store in stores:
+            url = store.get("url")
+            print(url)
